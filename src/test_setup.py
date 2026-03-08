1f33154b0753fc0d77c706ed42a0e2a06e3af827
anky32 <pandeyarpan84@gmail.com>
2026-03-08 21:26:48 +0545

    Add NEPSE cleaning script and requirements

---
+++ src/test_setup.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+print("Environment working")
+print("Pandas version:", pd.__version__)
+print("TensorFlow version:", tf.__version__)
