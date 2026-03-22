9570b4b8e50c32a620d3de529bd847a4e2075ee2
anky32 <pandeyarpan84@gmail.com>
2026-03-22 23:29:30 +0545

    Delete src directory

--- src/test_setup.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-
-print("Environment working")
-print("Pandas version:", pd.__version__)
-print("TensorFlow version:", tf.__version__)
